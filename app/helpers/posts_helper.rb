7108297410e11fa09b8b2f20ce555305d830ecc7
Heather Christman <efl7a@yahoo.de>
2018-02-20 14:14:08 -0500

    Done.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,3 +1,10 @@
 module PostsHelper
-  
+  def author_id_field(post)
+    if post.author.nil?
+      select_tag "post[author_id]",
+      options_from_collection_for_select(@authors, :id, :name)
+    else
+      hidden_field_tag "post[author_id]", post.author_id
+    end
+  end
 end
